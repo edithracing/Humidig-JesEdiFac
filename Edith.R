c232511d6b48ae9a3e81917a0ffc9a9d4e3e0ccf
edithracing <edithsuarez28@gmail.com>
2024-07-08 11:20:22 -0300

    Add files via upload

--- Edith.R
+++ Edith.R
@@ -1,19 +1,19 @@
+library(tidyverse)#llamo desde la libreria a tidyverse
+provincias <- read.csv('./Provincias.csv')#convoco al archivo csv previamente cargado
 
-library(leaflet) #llamo a la librería que cree al principio
+provincias <- provincias %>%
+  rename(situacion_calle = Personas.en.situación.de.calle..vía.pública.)
 
-# Definimos la latitud y longitud de la ubicación específica, para ello fui a Google maps y utilicé mis coordenadas
+# Calculo la media de personas en situación de calle por provincia
+media_situacion_calle <- provincias %>%
+  group_by(Nombre.de.provincia) %>% #Agrupo los datos por la columna Nombre.de.provincia.
+  summarise(media_situacion_calle = mean(situacion_calle, na.rm = TRUE)) #Calculo la media de la columna situacion_calle por cada provincia agrupada, generamos una nueva columna 'media_situacion_calle'
 
-lat <- -34.64 #indico la latitud
-
-long <- -58.60 #indico la longitud
-
-# Asignamos a una nueva variable mapa los datos de mi ubicación
-
-mapa <- leaflet() #creo el objeto mapa para que luego muestre el mapa interactivo.
-
-mapa <- setView(mapa, lng = long, lat = lat, zoom = 12) #Aquí establecemos la vista inicial del mapa, centramos en una ubicación específica, y le indicamos el nivel de zoom
-
-mapa <- addTiles(mapa) #con este comando indicamos que  agregue el tipo de fondo de mapa, esta librería cuenta con varios proveedores y estilos que permiten personalizar el fondo del mapa
-
-# Muestra nuestro mapa interactivo
-mapa
+# Creamos el gráfico de personas en situación de calle por provincias
+ggplot(provincias, aes(x = reorder(Nombre.de.provincia, situacion_calle), y = situacion_calle)) + #indico que datos asignamos a cada eje
+  geom_bar(stat = "identity", na.rm = TRUE) + #el tipo de gráfico que realicé, podría 
+  coord_flip() +  # Giro el gráfico para mayor legibilidad
+  labs(title = "Personas en situaciones de calle por Provincias argentinas", #indico las etiquetas
+       x = "Provincias",
+       y = "Cantidad de personas en situación de calle") +
+  theme_light() #indico el tema, ggplot tiene 8 temas, el genérico es minimal
